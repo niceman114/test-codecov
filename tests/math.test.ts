777644f976cf3abe8375ceacb8c41d2bbe57d74c
niceman114 <niceman114@gmail.com>
2025-01-03 09:53:10 +0900

    :beers: test: Add sample codes

---
+++ tests/math.test.ts
@@ -0,0 +1,13 @@
+import { add, multiply } from '../src/math';
+
+describe('Math Functions', () => {
+  test('add should return the sum of two numbers', () => {
+    expect(add(2, 3)).toBe(5);
+    expect(add(-1, 1)).toBe(0);
+  });
+
+  test('multiply should return the product of two numbers', () => {
+    expect(multiply(2, 3)).toBe(6);
+    expect(multiply(-2, 3)).toBe(-6);
+  });
+});

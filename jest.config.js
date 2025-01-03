96e88fdda9c3c98a700236c45f6843cc8dea291b
niceman114 <niceman114@gmail.com>
2025-01-03 16:50:58 +0900

    :construction_worker: ci: Fix jest junit configuration

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,15 @@ export default {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ['**/tests/**/*.test.ts'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: './coverage',
+        outputName: 'junit.xml',
+        suiteName: 'Jest Tests',
+      },
+    ],
+  ],
 };

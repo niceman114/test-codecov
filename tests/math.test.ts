5ef2b113a8c1a768c71ba9441496b0880f08d6ec
niceman114 <niceman114@gmail.com>
2025-01-03 11:33:40 +0900

    ✅ test: Make some test case excluded

--- tests/math.test.ts
+++ tests/math.test.ts
@@ -6,8 +6,8 @@ describe('Math Functions', () => {
     expect(add(-1, 1)).toBe(0);
   });
 
-  test('multiply should return the product of two numbers', () => {
-    expect(multiply(2, 3)).toBe(6);
-    expect(multiply(-2, 3)).toBe(-6);
-  });
+  // test('multiply should return the product of two numbers', () => {
+  //   expect(multiply(2, 3)).toBe(6);
+  //   expect(multiply(-2, 3)).toBe(-6);
+  // });
 });
